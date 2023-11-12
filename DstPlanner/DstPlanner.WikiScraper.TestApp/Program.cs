79ec0df4790f1424b0442426d01df6e584327cab
James Duncan <notprovided@notprovided>
2023-11-12 16:18:55 +1100

    Basic run setup

---
+++ DstPlanner/DstPlanner.WikiScraper.TestApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using DstPlanner.WikiScraper.Gpt3;
+
+// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Starting Wiki Scraper");
+
+var scraper = new Scraper();
+var crockPot = scraper.ParseGameItem("https://dontstarve.wiki.gg/wiki/Crock_Pot");
+
+Console.WriteLine("Result:");
+
+Console.WriteLine($"\tName: {crockPot.Name}");
+Console.WriteLine($"\tImage: {crockPot.ImgUrl ?? "None"}");
+
+if (crockPot.Ingredients != null)
+{
+    Console.WriteLine($"\tIngredients: {crockPot.ImgUrl}");
+    foreach (var ingredient in crockPot.Ingredients)
+    {
+        Console.WriteLine($"\t\t{ingredient.Name}");
+    }
+}
+else
+{
+    Console.WriteLine($"\tIngredients: None");
+}
+
--- DstPlanner/Gpt3-DstWikiScraper/GameItem.cs
+++ DstPlanner/Gpt3-DstWikiScraper/GameItem.cs
@@ -5,20 +5,20 @@ public record GameItem
     /// <summary>
     /// The name of the GameItem.
     /// </summary>
-    string Name { get; set; }
+    public string Name { get; set; }
 
     /// <summary>
     /// Link to the image of the game item.
     /// </summary>
-    string ImgUrl { get; set; }
+    public string ImgUrl { get; set; }
 
     /// <summary>
     /// List of game items required to craft this game item.
     /// </summary>
-    List<GameItem>? Ingredients { get; set; }
+    public List<GameItem>? Ingredients { get; set; }
 
     /// <summary>
     /// Required tools to craft this game item.
     /// </summary>
-    GameItem? Prerequisite { get; set; }
+    public GameItem? Prerequisite { get; set; }
 }
